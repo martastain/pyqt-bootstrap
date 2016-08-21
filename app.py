c444cabb089295b6089a16da98f502e8c55a0278
Martastain <martas@imm.cz>
2016-08-21 08:20:33 +0200

    added demo app

---
+++ app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+from pyqtbs import *
+
+class MainWidget(QWidget):
+    def __init__(self, parent):
+        super(QWidget, self).__init__(parent)
+        label = QLabel("Hello world")
+        layout = QVBoxLayout()
+        layout.addWidget(label)
+        self.setLayout(layout)
+
+
+if __name__ == "__main__":
+    app = Application(
+            name="qtdemo",
+            title="Cute demo"
+        )
+
+    window = MainWindow(app, MainWidget)
+    app.exec_()
+
